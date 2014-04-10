9599ef13975d345327e1bc0755be5b5eaefaa03a
Nick Seville <nickseville@gmail.com>
2014-04-10 12:57:02 -0400

    added script for question1.php

---
+++ question1.php
@@ -0,0 +1,35 @@
+<?php
+	// WARNING -- Please do not view before you solve the problems on your own, you are cheating yourself out of
+	// a good learning experience.
+
+	// #1 - If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	// Find the sum of all the multiples of 3 or 5 below 1000.
+	
+	/**
+	 * Finds the sum of all the numbers divisible by 3 or 5 under a specified number
+	 * @param  Max    $max  The upper bound number.
+	 * @return Status
+	 */ 
+	function question1($max) {	
+		$sum = 0;
+		$num = 0;
+
+		while ($num < $max) {
+		$num++;
+			if ($num % 3 == 0) {
+				$sum = $sum + $num;
+				
+			} elseif ($num % 5 == 0) { 
+				$sum = $sum + $num;
+				
+			}
+
+		}
+
+		echo $sum;
+
+	}
+
+	question1(999);
+
+?>
